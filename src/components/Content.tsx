60f985ed2ff39c7e00b9ebb97f6406c2bbdf73d2
Hugo <hugolacerdar@gmail.com>
2021-09-24 11:29:34 -0300

    Add Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenre: string;
+  movies: MovieProps[]
+}
+
+export function Content(props: ContentProps) {
+
+  const {selectedGenre, movies} = props;
+  
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
